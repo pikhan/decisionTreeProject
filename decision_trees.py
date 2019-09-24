75ae8cedeba13da3bbf8bed93a813601bdb4fce9
Malighchi <31544591+Malighchi@users.noreply.github.com>
2019-09-24 12:00:00 -0700

    Add files via upload
    Added a new parameter to the tree called label which returns the predicted label, and implemented test and test best

--- decision_trees.py
+++ decision_trees.py
@@ -25,6 +25,7 @@ class Node(object):
         self.samples = None
         self.feature_list = None
         self.feature_split = None
+        self.label = None
 
 
 
@@ -68,8 +69,56 @@ def DT_test_binary(X = None, Y = None, DT = None):
         print('\n The X 2D Array is empty. \n')
     if Y is None:
         print('\n The Y 2D Array is empty. \n')
+    if DT is not None:
+        if DT.left_child is None and DT.right_child is None:
+            int accuracy = 0;
+            for labels in Y:
+                if Y[labels] == DT.label:
+                    accuracy++
+            return (accuracy/range(Y))
+        else:
+            Xleft = np.array
+            Yleft = np.array
+            Xright = np.array
+            Yright = np.array
+            for sample in X:
+                if X[sample][DT.feature_split] == 0:
+                    Xleft.add(X[sample])
+                    Yleft.add(Y[sample])
+                else:
+                    Xright.add(X[sample])
+                    Yright.add(Y[sample])
+
+            return DT_test_binary(Xleft, Yleft, DT.left_child) + DT_test_binary(Xright, Yright, DT.right_child)
+
+def maxDepth(DT = None):
+    if node is None: 
+        return 0 ;  
+    else : 
+        lDepth = maxDepth(node.left) 
+        rDepth = maxDepth(node.right) 
+  
+        # Use the larger one 
+        if (lDepth > rDepth): 
+            return lDepth+1
+        else: 
+            return rDepth+1
 
 def DT_train_binary_best(X_train = None, Y_train = None, X_val = None, Y_val = None):
+    DT_best = DT_train_binary(X_train, Y_train, -1)
+    acc_best = DT_test_binary(X_val, Y_val, DT_best)
+    int depth = maxDepth(DT_best)
+
+    for x in range(depth):
+        DT_curr = DT_train_binary(X_train, Y_train, x)
+        acc_curr = DT_test_binary(X_val, Y_val, DT_curr)
+        if acc_curr >= acc_best:
+            acc_best = acc_curr
+            DT_best = DT_curr
+
+    return DT_best
+
+
 
 def DT_make_prediction(x = 0, DT = None):
     if x == 0:
